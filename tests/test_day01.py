0fadb4816f1e0442eda79a622d808c22d8660501
Daniel Mies <daniel.mies@codecentric.de>
2019-12-07 21:23:14 +0100

    renamed input variable

--- tests/test_day01.py
+++ tests/test_day01.py
@@ -4,43 +4,43 @@ from day01 import calculate_fuel_needed, calculate_fuel_for_fuel
 class TestDay01:
     def test_calculate_fuel_needed_caculates_correct_fuel_for_mass_12(self):
         expected = 2
-        input = 12
-        result = calculate_fuel_needed(input)
+        mass = 12
+        result = calculate_fuel_needed(mass)
         assert expected == result
 
     def test_calculate_fuel_needed_caculates_correct_fuel_for_mass_14(self):
         expected = 2
-        input = 14
-        result = calculate_fuel_needed(input)
+        mass = 14
+        result = calculate_fuel_needed(mass)
         assert expected == result
 
     def test_calculate_fuel_needed_caculates_correct_fuel_for_mass_1969(self):
         expected = 654
-        input = 1969
-        result = calculate_fuel_needed(input)
+        mass = 1969
+        result = calculate_fuel_needed(mass)
         assert expected == result
 
     def test_calculate_fuel_needed_caculates_correct_fuel_for_mass_100756(self):
         expected = 33583
-        input = 100756
-        result = calculate_fuel_needed(input)
+        mass = 100756
+        result = calculate_fuel_needed(mass)
         assert expected == result
 
     def test_calculate_fuel_for_fuel_caculates_correct_fuel_for_mass_14(self):
         expected = 2
-        input = 14
-        result = calculate_fuel_for_fuel(input)
+        mass = 14
+        result = calculate_fuel_for_fuel(mass)
         assert expected == result
 
     def test_calculate_fuel_for_fuel_caculates_correct_fuel_for_mass_1969(self):
         expected = 966
-        input = 1969
-        result = calculate_fuel_for_fuel(input)
+        mass = 1969
+        result = calculate_fuel_for_fuel(mass)
         assert expected == result
 
     def test_calculate_fuel_for_fuel_caculates_correct_fuel_for_mass_100756(self):
         expected = 50346
-        input = 100756
-        result = calculate_fuel_for_fuel(input)
+        mass = 100756
+        result = calculate_fuel_for_fuel(mass)
         assert expected == result
 
