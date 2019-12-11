e5c9aaf78d604608a6d36f19bf25dbd292f8c3c0
Daniel Mies <daniel.mies@codecentric.de>
2019-12-11 20:07:34 +0100

    refactored code

--- day11.py
+++ day11.py
@@ -1,98 +1,113 @@
 from util import filehelper
 from ship.computer import IntComputer
+import enum
 
 
-def paint(hull, coordinates, color):
-    x, y = coordinates
-    if x < hull["min_width"]:
-        hull["min_width"] = x
-    if y < hull["min_height"]:
-        hull["min_height"] = y
-    if x > hull["width"]:
-        hull["width"] = x
-    if y > hull["height"]:
-        hull["height"] = y
-    hull[coordinates] = color
-    return hull
+class Turn(enum.IntEnum):
+    LEFT = 0
+    RIGHT = 1
 
 
-def get_color(hull, coordinates):
-    if not coordinates in hull:
-        hull[coordinates] = 0
+class Direction(enum.IntEnum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
 
-    return hull[coordinates]
+    def turn(self, turn: Turn):
+        # dir 0: left 1: right
+        if turn == Turn.LEFT and self == Direction.UP:
+            return Direction.LEFT
+        elif turn == Turn.LEFT:
+            return Direction(int(self) - 1)
+        elif turn == Turn.RIGHT and self == Direction.LEFT:
+            return Direction.UP
+        else:
+            return Direction(int(self) + 1)
+
+    def move_in_direction(self, coordinates: (int, int)) -> (int, int):
+        x, y = coordinates
+        if self == Direction.UP:
+            y += 1
+        elif self == Direction.RIGHT:
+            x += 1
+        elif self == Direction.DOWN:
+            y -= 1
+        elif self == Direction.LEFT:
+            x -= 1
+        return (x, y)
 
 
-def paint_hull(robot, computer, hull):
+class Hull:
+    min_width = 0
+    width = 0
+    min_height = 0
+    height = 0
+    data = {}
 
+    def paint(self, coordinates: (int, int), color: int) -> None:
+        x, y = coordinates
+        self.min_width = min(x, self.min_width)
+        self.min_height = min(y, self.min_height)
+        self.width = max(x, self.width)
+        self.height = max(y, self.height)
+        self.data[coordinates] = color
+
+    def get_color(self, coordinates: (int, int)) -> int:
+        if not coordinates in self.data:
+            self.data[coordinates] = 0
+        return self.data[coordinates]
+
+    def get_size(self):
+        return len(self.data)
+
+    def print(self) -> None:
+        for y in range(self.height, self.min_height - 1, -1):
+            line = ""
+            for x in range(self.min_width, self.width):
+                item = " "
+                coordinates = (x, y)
+                if self.get_color(coordinates) == 1:
+                    item = "#"
+                else:
+                    item = " "
+                line += item
+            print(line)
+
+
+def paint_hull(robot, computer, hull: Hull) -> Hull:
     coordinates, direction = robot
     while not computer.finished:
-        current_color = get_color(hull, coordinates)
+        current_color = hull.get_color(coordinates)
         computer.inputs = [current_color]
         _, color = computer.run()
-        hull = paint(hull, coordinates, color)
+        hull.paint(coordinates, color)
 
         if not computer.finished:
             _, turn = computer.run()
+            direction = Direction(direction).turn(Turn(turn))
+            coordinates = Direction(direction).move_in_direction(coordinates)
 
-            # 0 = UP 1 = RIGHT 2=DOWN 3 = LEFT
-            if turn == 0:  # left
-                if direction == 0:
-                    direction = 3
-                else:
-                    direction -= 1
-            if turn == 1:  # right
-                if direction == 3:
-                    direction = 0
-                else:
-                    direction += 1
-            x, y = coordinates
-            if direction == 0:
-                y += 1
-            elif direction == 1:
-                x += 1
-            elif direction == 2:
-                y -= 1
-            elif direction == 3:
-                x -= 1
-            coordinates = (x, y)
     return hull
 
 
 def day11_01():
     program = filehelper.get_number_list_from_file("./puzzles/11/puzzle.txt")
-    robot = ((0, 0), 0)
+    robot = ((0, 0), Direction.UP)
     computer = IntComputer(program, inputs=[0], wait_after_output=True)
-    hull = {"min_width": 0, "min_height": 0, "width": 0, "height": 0}
+    hull = Hull()
     hull = paint_hull(robot, computer, hull)
-    # len -2 because we store width and height here, too
-    print(f"number of painted tiles: {len(hull)-2}")
+    print(f"number of painted tiles: {hull.get_size()}")
 
 
 def day11_02():
     program = filehelper.get_number_list_from_file("./puzzles/11/puzzle.txt")
     robot = ((0, 0), 0)
     computer = IntComputer(program, wait_after_output=True)
-    hull = {"min_width": 0, "min_height": 0, "width": 0, "height": 0}
-    hull = paint(hull, (0, 0), 1)
+    hull = Hull()
+    hull.paint((0, 0), 1)
     hull = paint_hull(robot, computer, hull)
-    # len -4 because we store width and height here, too
-    min_width = hull["min_width"]
-    width = hull["width"]
-    min_height = hull["min_height"]
-    height = hull["height"]
-
-    for y in range(height, min_height - 1, -1):
-        line = ""
-        for x in range(min_width, width):
-            item = " "
-            coordinates = (x, y)
-            if get_color(hull, coordinates) == 1:
-                item = "#"
-            else:
-                item = " "
-            line += item
-        print(line)
+    hull.print()
 
 
 if __name__ == "__main__":
